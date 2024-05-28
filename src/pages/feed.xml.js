87f023d3c42e027af7c78616025bd93b4b913572
Ramkarthik Krishnamurthy <ramkarthikkrishnamurthy@gmail.com>
2024-05-28 18:10:36 +0530

    Add content for RSS

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,11 +1,14 @@
 import { AppConfig } from "@/utils/AppConfig";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET() {
   const blog = await getCollection("essays");
   return rss({
-    title: AppConfig.title,
+    title: "Ramkarthik Krishnamurthy's Essays",
     description: AppConfig.description,
     site: AppConfig.site,
     items: blog.map((post) => ({
@@ -13,6 +16,9 @@ export async function GET() {
       pubDate: post.data.createdDate,
       description: post.data.summary,
       link: `/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
   });
 }
