aa690875e80bfc7eb90960824bbb565d7e2a82ab
Ramkarthik <ramkarthikkrishnamurthy@gmail.com>
2026-03-04 21:03:06 +0530

    Cleanup projects

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,50 +1,41 @@
 export const AppConfig = {
-        author: "Ramkarthik Krishnamurthy",
-        title: "I'm Ramkarthik Krishnamurthy and this is my personal website",
-        description: "Ramkarthik is a software engineer and he writes notes, essays, and code.",
-        image: "/images/social.png",
-        twitter: "@Ramkarthik",
-        site: "https://kramkarthik.com/",
-        pages: [{
-            name: "Essays",
-            link: "/articles"
-        },{
-            name: "Notes",
-            link: "/notes"
-        },{
-            name: "Programming",
-            link: "/programming"
-        },{
-            name: "Projects",
-            link: "/projects"
-        },{
-            name: "Lists",
-            link: "/lists"
-        }],
-        projects: [{
-            name: "glace",
-            link: "https://github.com/ramkarthik/glace",
-            summary: "An easy to use, high performance framework to quickly build APIs for the Gleam programming language."
-        },{
-            name: "pathern",
-            link: "https://github.com/ramkarthik/pathern",
-            summary: "A URL path pattern matching library for the Gleam programming language."
-        },{
-            name: "t",
-            link: "https://github.com/Ramkarthik/t",
-            summary: "A very minimal, CLI based todo app that is written in Rust."
-        },
-        {
-            name: "ZValidation",
-            link: "https://github.com/Ramkarthik/zvalidation",
-            summary: "Simple, fast, and extensible validation library for .NET. "
-        },{
-            name: "Markdown Notes",
-            link: "https://markdownnote.com/",
-            summary: "Markdown Notes is a free web app to instantly start taking notes."
-        },{
-            name: "HackerNews Deck",
-            link: "https://hackernewsdeck.vercel.app/",
-            summary: "A cleaner and more efficient way to browse HackerNews."
-        }]
-    }
+    author: "Ramkarthik Krishnamurthy",
+    title: "I'm Ramkarthik Krishnamurthy and this is my personal website",
+    description: "Ramkarthik is a software engineer and he writes notes, essays, and code.",
+    image: "/images/social.png",
+    twitter: "@Ramkarthik",
+    site: "https://kramkarthik.com/",
+    pages: [{
+        name: "Essays",
+        link: "/articles"
+    }, {
+        name: "Notes",
+        link: "/notes"
+    }, {
+        name: "Programming",
+        link: "/programming"
+    }, {
+        name: "Projects",
+        link: "/projects"
+    }, {
+        name: "Lists",
+        link: "/lists"
+    }],
+    projects: [{
+        name: "Blogosphere",
+        link: "https://blogosphere.app/",
+        summary: "Social media platforms deprioritize external links which makes it harder to find interesting blog posts. Blogosphere aggregates posts from over 1,000 blogs written by real humans."
+    }, {
+        name: "glace",
+        link: "https://github.com/ramkarthik/glace",
+        summary: "An easy to use, high performance framework to quickly build APIs for the Gleam programming language."
+    }, {
+        name: "pathern",
+        link: "https://github.com/ramkarthik/pathern",
+        summary: "A URL path pattern matching library for the Gleam programming language."
+    }, {
+        name: "HackerNews Deck",
+        link: "https://hackernewsdeck.vercel.app/",
+        summary: "A cleaner and more efficient way to browse HackerNews."
+    }]
+}
