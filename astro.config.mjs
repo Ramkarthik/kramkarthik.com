6505dd5a4552dbe8b7fac85144db8765f928c9c4
Ramkarthik <ramkarthikkrishnamurthy@gmail.com>
2026-03-14 08:15:08 +0530

    Adding a component to display 100DaysToOffload progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import remarkCodeTitles from "remark-code-titles";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kramkarthik.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   markdown: {
     syntaxHighlight: "prism",
     remarkPlugins: [remarkCodeTitles],
